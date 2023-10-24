e38e102bc251de5196acfc4a120b2458d090a883
Josefie Zynga <josefie.zynga@betterplace.org>
2023-10-24 20:23:14 +0100

    remove unnecessary stuff

--- postcss.config.js
+++ postcss.config.js
@@ -1,22 +1,9 @@
-const tokens = require('./tokens/json/core.json')
 const postcss = require('postcss')
-
 
 const base = {
   plugins: {
     'autoprefixer': {},
     'postcss-nesting': {},
-    'postcss-env-function': {
-      importFrom: () => {
-        const environmentVariables = {}
-        Object.entries(tokens).forEach(([key, entry]) => {
-          if (key.includes('betterplace-measure')) {
-            environmentVariables[`--${key}`] = entry
-          }
-        })
-        return { environmentVariables }
-      },
-    },
   },
 }
 
--- stories/MoneyInput.stories.tsx
+++ stories/MoneyInput.stories.tsx
@@ -12,9 +12,7 @@ export default {
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ['autodocs'],
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
+  argTypes: {},
 } satisfies Meta<typeof MoneyInput>
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
