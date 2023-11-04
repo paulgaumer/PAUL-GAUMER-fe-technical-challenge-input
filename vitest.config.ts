b360882a24f5b69dba444f771877a060dad3f9ce
Paul Gaumer <17173804+paulgaumer@users.noreply.github.com>
2023-11-04 21:58:36 +0100

    chor: fix vitest setup with RTL

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   test: {
+    globals: true,
+    environment: 'jsdom',
     setupFiles: ['./setupTests.ts'],
   },
   plugins: [tsconfigPaths(), react()],
